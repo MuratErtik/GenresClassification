5cfee74eba263baf2b1c80cba7bbbe504d4908ed
Murat Ertik <dmuratertik1@gmail.com>
2025-07-19 07:26:39 -0700

    count

---
+++ count.py
@@ -0,0 +1,12 @@
+import os
+
+path = "/Users/muratertik/Desktop/genresClassification/scifi5821" 
+
+jpg_count = len([file for file in os.listdir(path) if dosya.lower().endswith(".jpg")])
+
+print(f".jpg count: {jpg_sayisi}")
+#comedy 5136
+#action 5178
+#animation 5428
+#horror 5232
+#scifi 5520
